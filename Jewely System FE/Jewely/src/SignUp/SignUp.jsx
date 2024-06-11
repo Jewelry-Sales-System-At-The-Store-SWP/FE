087521a501a8bf1c1cf22c36a9a0a87f9790b8b8
ngckinhluan <nkluantran2907@gmail.com>
2024-06-11 12:30:05 +0700

    update signup

--- Jewely System FE/Jewely/src/SignUp/SignUp.jsx
+++ Jewely System FE/Jewely/src/SignUp/SignUp.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
   const navigate = useNavigate();
+
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
 
   const navigateToSignIn = () => {
     navigate("/login");
@@ -15,40 +20,80 @@ const SignUp = () => {
           <form className="form-login-have-account">
             <div className="email">
               <div className="email-label-wrapper">
-                <div className="label">Email</div>
+                <div className="label">What should we call you?</div>
                 <div className="password-hide-see">
                   <img className="icon" alt="" />
                   <div className="hide">Hide</div>
                 </div>
               </div>
-              <input className="text-field" type="text" />
+              <input
+                className="text-field"
+                type="text"
+                placeholder="Enter your profile name here"
+              />
               <div className="error-message">Error message</div>
             </div>
             <div className="password-field-wrapper">
               <div className="text-field1">
                 <div className="password-label-wrapper">
-                  <div className="label1">Phone</div>
+                  <div className="label1">What's your email?</div>
 
                   <div className="password-hide-see1" onClick="">
                     <div className="password-icon-wrapper"></div>
                     <div className="hide1"></div>
                   </div>
                 </div>
-                <input className="text-field2" type="" />
+                <input
+                  className="text-field2"
+                  type=""
+                  placeholder="Enter your email address"
+                />
                 <div className="error-message1">Error message</div>
               </div>
             </div>
             <div className="password-field-wrapper">
               <div className="text-field1">
                 <div className="password-label-wrapper">
-                  <div className="label1">Password</div>
-
-                  <div className="password-hide-see1" onClick="">
+                  <div className="label1">Create a password</div>
+                  <div
+                    className="password-hide-see1"
+                    onClick={togglePasswordVisibility}
+                  >
+                    <div className="password-icon-wrapper">
+                      {passwordVisible ? (
+                        <img
+                          className="icon1"
+                          loading="lazy"
+                          alt=""
+                          src="./src/assets/icon-1.svg"
+                        />
+                      ) : (
+                        <img
+                          className="icon1"
+                          loading="lazy"
+                          alt=""
+                          src="./src/assets/show.svg"
+                        />
+                      )}
+                    </div>
+                    <div className="hide1">
+                      {passwordVisible ? "Hide" : "Show"}
+                    </div>
+                  </div>
+                  <div
+                    style={{ marginLeft: "-380px" }}
+                    className="password-hide-see1"
+                    onClick=""
+                  >
                     <div className="password-icon-wrapper"></div>
                     <div className="hide1"></div>
                   </div>
                 </div>
-                <input className="text-field2" type="" />
+                <input
+                  className="text-field2"
+                  type={passwordVisible ? "text" : "password"}
+                  placeholder="Enter your password"
+                />
                 <div className="error-message1">Error message</div>
               </div>
             </div>
