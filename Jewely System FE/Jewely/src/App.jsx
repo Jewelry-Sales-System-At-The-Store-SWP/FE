1e901a614d0697778c02c5cfc8385993b2e38fac
TranHuyHoang1509 <hugh.g.tran@gmail.com>
2024-06-12 13:37:38 +0700

    updated Add Staff and style for Staff

--- Jewely System FE/Jewely/src/App.jsx
+++ Jewely System FE/Jewely/src/App.jsx
@@ -4,11 +4,19 @@ import React from 'react'
 import ManageJewely from './ManageJewely/ManageJewely'
 import ManagePromotion from './ManagePromotion/ManagePromotion'
 import InvoiceTemplate from './InvoiceTemplate/InvoiceTemplate'
-import InvoiceAdmin from './InvoiceAdmin/InvoiceAdmin'
 import WarrantyCertificate from './WarrantyCertificate/WarrantyCertificate'
 import ManageCustomer from './ManageCustomer/ManageCustomer'
 import Dashboard from "./Dashboard/Dashboard"
+import ManageStaff from './ManageStaff/ManageStaff'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import InvoiceAdminPage from './InvoiceAdmin/InvoiceAdmin'
+import AddPromotion from './AddPromotion/AddPromotion'
+import EditPromotion from './EditPromotion/EditPromotion'
+import AddCustomer from './AddCustomer/AddCustomer'
+import EditCustomer from './EditCustomer/EditCustomer'
+import AddJewelry from './AddJewelry/AddJewelry'
+import EditJewelry from './EditJewelry/EditJewelry'
+import AddStaff from './AddStaff/AddStaff'
 
 export default function App() {
   return (
@@ -17,12 +25,21 @@ export default function App() {
       <Routes>
         <Route path="/manage-jewelry" element={<ManageJewely />} />
         <Route path="/manage-promotion" element={<ManagePromotion />} />
+        <Route path="/manage-staff" element={<ManageStaff />} />
         <Route path="/invoice-template" element={<InvoiceTemplate />} />
-        <Route path="/invoice-admin" element={<InvoiceAdmin />} />
+        <Route path="/invoice-sell" element={<InvoiceAdminPage />} />
         <Route path="/warranty-certificate" element={<WarrantyCertificate />} />
         <Route path="/manage-custom" element={<ManageCustomer />} />       
+        <Route path="/manage-staff" element={<ManageStaff />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/" element={<Dashboard />} /> 
+        <Route path="/" element={<Dashboard />} />
+        <Route path='/add-promotion' element={<AddPromotion />} />
+        <Route path='/edit-promotion/:id' element={<EditPromotion />} />
+        <Route path='/add-customer' element={<AddCustomer />} />
+        <Route path='/edit-customer/:id' element={<EditCustomer />} />
+        <Route path='/add-jewelry' element={<AddJewelry />} />
+        <Route path='/edit-jewelry/:id' element={<EditJewelry />} />
+        <Route path='/add-staff' element={<AddStaff />} />
       </Routes>
     </div>
   </Router>
--- Jewely System FE/Jewely/src/ManageStaff/ManageStaffData.jsx
+++ Jewely System FE/Jewely/src/ManageStaff/ManageStaffData.jsx
@@ -2,67 +2,43 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./ManageStaff.css";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import axios from "axios";
+import ReactPaginate from "react-paginate";
+
 const StaffDataTable = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 5;
 
   useEffect(() => {
     axios
       .get("http://localhost:5188/api/User/GetUsers")
       .then((response) => {
         setData(response.data);
-        console.log(response.data);
       })
       .catch((error) => {
         console.error("Error fetching promotion details:", error);
       });
   }, []);
-  
-  const handleDelete = (userId) => {
-    Swal.fire({
-      title: "Are you sure that you want to delete this customer?",
-      text: "You won't be able to revert this!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!",
-      
-    }).then((result) => {
-      if (result.isConfirmed) {
-        axios
-          .delete(`http://localhost:5188/api/User/DeleteUser/${userId}`)
-          .then(() => {
-            Swal.fire({
-              title: "Deleted!",
-              text: "Your staff has been deleted.",
-              icon: "success",
-            }).then(() => {
-              Swal.fire({
-                position: "top-end",
-                icon: "success",
-                title: "Your work has been saved",
-                showConfirmButton: false,
-                timer: 1500,
-              });
-            });
-            setPromotion(
-              data.filter((item) => item.customerId !== customerId)
-            );
-          })
-          .catch((error) => {
-            console.error("There was an error deleting the user:", error);
-            Swal.fire({
-              title: "Error!",
-              text: "There was an error deleting the user.",
-              icon: "error",
-            });
-          });
-      }
-    });
+const navigate = useNavigate();
+
+const handleClick = () => {
+  navigate("/add-staff");
+};
+
+  const handlePageClick = (event) => {
+    setCurrentPage(event.selected);
   };
+
+  const offset = currentPage * itemsPerPage;
+  const currentPageData = data.slice(offset, offset + itemsPerPage);
+  const pageCount = Math.ceil(data.length / itemsPerPage);
+
+  
   return (
+    
     <main className="main">
       <section className="absolute top-[89px] left-[357px] shadow-[0px_10px_60px_rgba(226,_236,_249,_0.5)] rounded-11xl bg-white w-[968px] flex flex-row items-start justify-between pt-8 pb-[25px] pr-[74px] pl-[50px] box-border gap-[20px] max-w-full text-left text-sm text-darkgray font-poppins mq700:flex-wrap">
         <img
@@ -107,8 +83,8 @@ const StaffDataTable = () => {
         </div>
       </section>
 
-      <section className="absolute top-[280px] left-[326px] shadow-[0px_6.3px_37.5px_rgba(226,_236,_249,_0.5)] rounded-[18.75px] bg-white-lighter w-[1041.5px] h-[600px] items-start justify-start max-w-full">
-        <form className="m-0 flex-1 shadow-[0px_6.3px_37.5px_rgba(226,_236,_249,_0.5)] rounded-[18.75px] bg-white flex flex-col items-start justify-start pt-[13.3px] px-0 pb-[42px] box-border gap-[33.4px] max-w-full z-[3]">
+      <section className="absolute top-[285px] left-[326px] shadow-[0px_6.3px_37.5px_rgba(226,_236,_249,_0.5)] rounded-[18.75px] bg-white w-[1041.5px] flex flex-row items-start justify-start max-w-full" style={{ minHeight: '600px' }}>
+        <form className="m-0 flex-1 shadow-[0px_6.3px_37.5px_rgba(226,_236,_249,_0.5)] rounded-[18.75px] bg-white flex flex-col items-start justify-start pt-[13.3px] px-0 pb-[42px] box-border gap-[33.4px] max-w-full z-[3]" style={{ minHeight: '600px' }}>
           <img
             className="self-stretch h-[631px] relative rounded-[18.75px] max-w-full overflow-hidden shrink-0 hidden"
             alt=""
@@ -124,49 +100,56 @@ const StaffDataTable = () => {
                   Active Staff
                 </div>
               </div>
-
               {data && (
-  <table className="absolute w-[813px] h-[200px] top-[150px] left-[25px]">
-    <tbody>
-      {data
-        .filter((item) => item.role.roleName === "Staff")
-        .map((item, index) => (
-          <tr key={index} className="staff-table">
-            <td className="absolute left-[0px] text-black inline-block w-[200px]">
-              {item.username}
-            </td>
-            <td className="absolute left-[203px]">{item.counter}1</td>
-            <td className="absolute left-[300px]">phone</td>
-            <td className="absolute left-[495px]">{item.email}</td>
-            <td className="absolute left-[680px]">revenue</td>
-            <td className="relative">
-              <div className="btn-wrapper">
-                <button className="btn-more-info">Active</button>
-              </div>
-            </td>
-            <td className="relative">
-              <div className="btn-wrapper" >
-                <button className="btn-more-info w-[60px] round-full absolute left-[540px] ">Edit</button>
-                <button 
-                 onClick={() => handleDelete(item.userId)}
-                className="btn-more-info absolute left-[400px] w-[30px]  round-full bg-red-300 text-red-700  border-red-500"
-                >
-                  X</button>
-              </div>
-            </td>
-            
-          </tr>
-        ))}
-    </tbody>
-  </table>
-)}
+                <table className="absolute w-[813px] h-[200px] top-[150px] left-[25px]">
+                  <tbody>
+                    {data
+                      .filter((item) => item.role.roleName === "Staff")
+                      .map((item, index) => (
+                        <tr key={index} className="staff-table">
+                          <td className="absolute left-[0px] text-black inline-block w-[200px]">
+                            {item.username}
+                          </td>
+                          <td className="absolute left-[203px]">
+                            {item.counter}1
+                          </td>
+                          <td className="absolute left-[300px]">phone</td>
+                          <td className="absolute left-[495px]">
+                            {item.email}
+                          </td>
+                          <td className="absolute left-[680px]">revenue</td>
+                          <td className="relative">
+                            <div className="btn-wrapper">
+                              <button className="btn-more-info">Active</button>
+                            </div>
+                          </td>
+                          <td className="relative">
+                            <div className="btn-wrapper">
+                              <button className="btn-more-info w-[60px] round-full absolute left-[540px] ">
+                                Edit
+                              </button>
+                              <button
+                                
+                                className="btn-more-info absolute left-[400px] w-[30px]  round-full bg-red-300 text-red-700  border-red-500"
+                              >
+                                X
+                              </button>
+                            </div>
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
+              )}
 
 
-              <div className="absolute ml-50 top-[29.3px] left-[750px] rounded-[6.25px] bg-ghostwhite-200 w-[229.8px] h-[34.6px] z-[1] text-5xs-5 text-silver">
+              <div className="absolute ml-50 top-[37.3px] left-[571.1px] rounded-[6.25px] bg-ghostwhite-200 w-[229.8px] h-[34.6px] z-[1] text-5xs-5 text-silver">
                 <div className="absolute top-[0px] left-[0px] rounded-[6.25px] bg-ghostwhite-100 w-full h-full hidden" />
-                <div className="absolute top-[12.2px] left-[31.7px] tracking-[-0.01em] inline-block w-[44.5px] h-[13.9px] z-[1]">
-                  Search
-                </div>
+                <input
+                  type="text"
+                  className="absolute top-[12.2px] left-[41.7px] tracking-[-0.01em] inline-block w-[150px] h-[13.9px] z-[1] border-none outline-none bg-transparent text-black placeholder-black"
+                  placeholder="Search"
+                />
                 <img
                   className="absolute top-[9.2px] left-[8.9px] w-[20.9px] h-[16.3px] overflow-hidden z-[1]"
                   loading="lazy"
@@ -175,17 +158,20 @@ const StaffDataTable = () => {
                 />
               </div>
 
-              <div className="flex w-[97px] h-[28px] items-center justify-center gap-[6.25px] px-[7.5px] py-[2.5px] bg-[#15c09861] rounded-[2.5px] border-[0.63px] mr-[0px] border-solid border-[#00b086] absolute left-[578px]">
-                <div className="relative w-fit font-medium text-[#008667] text-[8.8px] tracking-[-0.09px] leading-[normal]">
-                  Add
+              <div className="absolute top-[37.3px] ml-42.5 left-[380px] flex gap-[6.25px]"> 
+                <div 
+                onClick={handleClick}
+                className="flex w-[97px] h-[28px] items-center justify-center gap-[6.25px] px-[7.5px] py-[2.5px] bg-[#15c09861] rounded-[2.5px] border-[0.63px] mr-[0px] border-solid border-[#00b086]">
+                  <div className="relative w-fit font-medium text-[#008667] text-[8.8px] tracking-[-0.09px] leading-[normal]">
+                    Add
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+          <div className="absolute top-[133px] left-[15px] box-border w-[1000px] h-[0.6px] z-[1] border-t-[0.6px] border-solid border-whitesmoke-200" />
 
-          <div className="absolute top-[115px] left-[15px] box-border w-[1000px] h-[0.6px] z-[1] border-t-[0.6px] border-solid border-whitesmoke-200" />
-
-          <div className="self-stretch flex flex-col items-start justify-start gap-[8.8px] max-w-full   ">
+          <div className="header data absolute top-[110px]">
             <div className="w-[1009.2px] flex flex-row items-start justify-start py-0 px-10 box-border max-w-full  ">
               <div className="w-[123.9px] relative text-4xs-8 tracking-[-0.01em] font-medium font-poppins text-silver whitespace-pre-wrap text-left inline-block shrink-0 z-[1]">
                 Staff Name
@@ -217,6 +203,19 @@ const StaffDataTable = () => {
           </div>
         </form>
       </section>
+      <ReactPaginate
+        previousLabel={"previous"}
+        nextLabel={"next"}
+        breakLabel={"..."}
+        breakClassName={"break-me"}
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination-staff"}
+        subContainerClassName={"pages pagination-staff"}
+        activeClassName={"active"}
+      />
     </main>
   );
 };
